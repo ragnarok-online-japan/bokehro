180241b47bdb179a44f996ff0513a8d739b2e03b
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-05-05 17:02:23 +0900

    [modify] Update scrapyd_setup.py

--- scrapyd_setup.py
+++ scrapyd_setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
+    name='bokehro-scrapyd',
     version='1.0',
     packages=find_packages(),
     include_package_data=True,  # 必要なデータを含める
